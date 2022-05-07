430cdac7a041de7efd78577871318b0be8636b20
ahmadadityakj <aditya.ahmad@gmail.com>
2022-05-08 06:45:22 +0700

    update bot

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -16,7 +16,6 @@ let postWebhook = (req, res) =>{
             console.log('webhook event');
             console.log(webhook_event);
 
-
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
@@ -77,39 +76,9 @@ function handleMessage(sender_psid, received_message) {
         response = {
             "text": `You sent the message: "${received_message.text}". Now send me an image!`
         }
-    } else if (received_message.attachments) {
+    } 
 
-      // Gets the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-          "attachment": {
-              "type": "template",
-              "payload": {
-                  "template_type": "generic",
-                  "elements": [{
-                      "title": "Is this the right picture?",
-                      "subtitle": "Tap a button to answer.",
-                      "image_url": attachment_url,
-                      "buttons": [
-                          {
-                              "type": "postback",
-                              "title": "Yes!",
-                              "payload": "yes",
-                          },
-                          {
-                              "type": "postback",
-                              "title": "No!",
-                              "payload": "no",
-                          }
-                      ],
-                  }]
-              }
-          }
-      }
-
-    }
-
-// Sends the response message
+    // Sends the response message
     callSendAPI(sender_psid, response);
 }
 
@@ -145,7 +114,7 @@ function callSendAPI(sender_psid, response) {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v7.0/me/messages",
+        "uri": "https://graph.facebook.com/v13.0/me/messages",
         "qs": { "access_token": process.env.FB_PAGE_TOKEN },
         "method": "POST",
         "json": request_body
@@ -157,97 +126,6 @@ function callSendAPI(sender_psid, response) {
         }
     });
 }
-
-// function firstTrait(nlp, name) {
-//     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
-// }
-
-function firstTrait(nlp, name) {
-    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
-}
-
-// function handleMessage(sender_psid, message) {
-//     //handle message for react, like press like button
-//     // id like button: sticker_id 369239263222822
-
-//     if( message && message.attachments && message.attachments[0].payload){
-//         callSendAPI(sender_psid, "Thank you for watching my video !!!");
-//         callSendAPIWithTemplate(sender_psid);
-//         return;
-//     }
-
-//     let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
-//     let entityChosen = "";
-//     entitiesArr.forEach((name) => {
-//         let entity = firstTrait(message.nlp, name);
-//         if (entity && entity.confidence > 0.8) {
-//             entityChosen = name;
-//         }
-//     });
-
-//     if(entityChosen === ""){
-//         //default
-//         callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
-//     }else{
-//        if(entityChosen === "wit$greetings"){
-//            //send greetings message
-//            callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
-//        }
-//        if(entityChosen === "wit$thanks"){
-//            //send thanks message
-//            callSendAPI(sender_psid,`You 're welcome!`);
-//        }
-//         if(entityChosen === "wit$bye"){
-//             //send bye message
-//             callSendAPI(sender_psid,'bye-bye!');
-//         }
-//     }
-// }
-
-let callSendAPIWithTemplate = (sender_psid) => {
-    // document fb message template
-    // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
-    let body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [
-                        {
-                            "title": "Want to build sth awesome?",
-                            "image_url": "https://www.nexmo.com/wp-content/uploads/2018/10/build-bot-messages-api-768x384.png",
-                            "subtitle": "Watch more videos on my youtube channel ^^",
-                            "buttons": [
-                                {
-                                    "type": "web_url",
-                                    "url": "https://bit.ly/subscribe-haryphamdev",
-                                    "title": "Watch now"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-    };
-
-    request({
-        "uri": "https://graph.facebook.com/v6.0/me/messages",
-        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
-        "method": "POST",
-        "json": body
-    }, (err, res, body) => {
-        if (!err) {
-            // console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
-};
 
 module.exports = {
   postWebhook: postWebhook,
