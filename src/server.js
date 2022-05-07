24bd38c1d5e8a2a47e506841c1dea14b817af07d
ahmadadityakj <aditya.ahmad@gmail.com>
2022-05-07 16:06:08 +0700

    initial

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import express from "express";
+import configView from "./config/view";
+import webRoute from "./routes/web";
+import bodyParser from "body-parser";
+
+const app = express();
+
+// setup view
+configView(app);
+
+//set body parser to post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// init routes
+webRoute(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+   console.log(`Running using port ${port}`) ;
+});
