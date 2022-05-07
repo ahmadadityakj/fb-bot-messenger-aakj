1c58d4565d917234600be6310a71c24e88e8307f
ahmadadityakj <aditya.ahmad@gmail.com>
2022-05-08 06:01:32 +0700

    update

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -120,12 +120,14 @@ function handlePostback(sender_psid, received_postback) {
     // Get the payload for the postback
     let payload = received_postback.payload;
 
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
+    switch (payload) {
+      case 'welcome':
+        response = { "text": "Hi" }
+        break;
+      default:
+        break;
     }
+
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
