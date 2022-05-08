78fd7d97984369cb4d2358aa9b00a2c10c410b88
ahmadadityakj <aditya.ahmad@gmail.com>
2022-05-09 06:35:30 +0700

    update bot

--- src/routes/web.js
+++ src/routes/web.js
@@ -15,9 +15,27 @@ let webRoutes = (app)=> {
     stormDb.emptyDatabase();
     res.status(200).send('ok');
   });
+  router.get("/dummydata", (req, res) => {
+    stormDb.db.get('messages').set([
+      { user: '12345', name: 'john', messages: [], state: 'welcome', birthDate: '' },
+      { user: '45678', name: 'robert', messages: [], state: 'birthdate', birthDate: '1990-01-30' },
+    ]);
+    res.status(200).send('ok');
+  });
   router.get("/messages", (req, res) => {
     const allMessages = stormDb.db.get('messages').value();
     res.status(200).json(allMessages);
+  });
+  router.get("/summary", (req, res) => {
+    const allMessages = stormDb.db.get('messages').value();
+    const summary = allMessages.map(x => {
+      return {
+        user: x.user,
+        name: x.name,
+        messages: x.messages
+      }
+    });
+    res.status(200).json(summary);
   });
   router.get("/messages/:id", (req, res) => {
     const params = req.params;
